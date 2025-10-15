90aecb70b27cee6bde8288e09ce613ee21382e51
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-10-16 00:23:26 +0300

    feat: repository get remote

--- app/src/main/java/com/ahmedhgabr/qurio/model/TriviaRepositoryImpl.kt
+++ app/src/main/java/com/ahmedhgabr/qurio/model/TriviaRepositoryImpl.kt
@@ -1,9 +1,13 @@
 package com.ahmedhgabr.qurio.model
 
+import com.ahmedhgabr.qurio.model.mapper.toCategory
+import com.ahmedhgabr.qurio.model.mapper.toQuestion
 import com.ahmedhgabr.qurio.model.remote.TriviaApi
 import com.ahmedhgabr.qurio.model.remote.TriviaApiService
 import com.ahmedhgabr.qurio.model.remote.dto.QuestionsResponse
 import com.ahmedhgabr.qurio.model.remote.responseWrapper
+import com.ahmedhgabr.qurio.presenter.entity.Category
+import com.ahmedhgabr.qurio.presenter.entity.question.Question
 import com.ahmedhgabr.qurio.presenter.repository.TriviaRepository
 
 class TriviaRepositoryImpl() : TriviaRepository {
@@ -18,4 +22,32 @@ class TriviaRepositoryImpl() : TriviaRepository {
         }
         return question.results.firstOrNull()?.question ?: "No question found"
     }
+
+    override suspend fun getToken(): String {
+        val tokenResponse = responseWrapper {
+            triviaApiService.getToken()
+        }
+        return tokenResponse.token
+    }
+
+    override suspend fun getCategories(): List<Category> {
+        val categoriesResponse = responseWrapper {
+            triviaApiService.getCategories()
+        }
+        return categoriesResponse.triviaCategories.map { categoryDTO -> categoryDTO.toCategory() }
+    }
+
+    override suspend fun getQuestions(
+        amount: Int,
+        category: Int?,
+        difficulty: String?
+    ): List<Question> {
+        val token = getToken()
+        val questionsResponse = responseWrapper {
+            triviaApiService.getQuestions(amount, category, difficulty, token)
+        }
+        return questionsResponse.results.map { questionDTO -> questionDTO.toQuestion() }
+    }
+
+
 }
