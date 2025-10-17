1ec2357f70a1e4dd1038a71f932528a7fbb3e631
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-10-17 18:18:14 +0300

    refactor: remove example from main activity

--- app/src/main/java/com/ahmedhgabr/qurio/MainActivity.kt
+++ app/src/main/java/com/ahmedhgabr/qurio/MainActivity.kt
@@ -4,41 +4,16 @@ import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import com.ahmedhgabr.qurio.databinding.ActivityMainBinding
-import com.ahmedhgabr.qurio.presenter.BasePresenter
-import com.ahmedhgabr.qurio.ui.BaseView
 
-class MainActivity : AppCompatActivity(), BaseView {
+class MainActivity : AppCompatActivity(){
 
     lateinit var binding: ActivityMainBinding
-    private val presenter = BasePresenter()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-        setup()
     }
-
-    private fun setup() {
-        presenter.view = this
-        presenter.getInfo()
-        binding.button.setOnClickListener {
-            presenter.getWisdom()
-        }
-    }
-
-    override fun onError(message: Throwable) {
-        binding.textView.text = getString(R.string.network_error)
-    }
-
-    override fun onInfoReceived(info: String) {
-        binding.textView.text = info
-    }
-
-    override fun onRandomInfoReceived(info: String) {
-        binding.textView.text = info
-    }
-
 
 }
