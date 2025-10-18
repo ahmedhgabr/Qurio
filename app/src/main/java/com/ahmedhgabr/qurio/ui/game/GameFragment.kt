26454ab0556f7a2535334491b8100298d45a924b
Ahmed Hussien <ahmedhgabr25@gmail.com>
2025-10-18 15:03:23 +0300

    feat: show question

--- app/src/main/java/com/ahmedhgabr/qurio/ui/game/GameFragment.kt
+++ app/src/main/java/com/ahmedhgabr/qurio/ui/game/GameFragment.kt
@@ -1,29 +1,50 @@
 package com.ahmedhgabr.qurio.ui.game
 
 import android.os.Bundle
-import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.ahmedhgabr.qurio.R
+import com.ahmedhgabr.qurio.QurioApplication
+import com.ahmedhgabr.qurio.databinding.FragmentGameBinding
+import com.ahmedhgabr.qurio.presenter.entity.question.Difficulty
+import com.ahmedhgabr.qurio.presenter.entity.question.Question
+import com.ahmedhgabr.qurio.presenter.game.GamePresenter
+import com.ahmedhgabr.qurio.presenter.game.GamePresenterI
+import com.ahmedhgabr.qurio.ui.BaseFragment
+import javax.inject.Inject
 
-class GameFragment : Fragment() {
+class GameFragment : BaseFragment<FragmentGameBinding, GameView, GamePresenter>(), GameView  {
+
+    @Inject
+    lateinit var presenter: GamePresenterI
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        (requireActivity().application as QurioApplication).appComponent.inject(this)
+        presenter.attachView(this)
     }
 
-    override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
-        savedInstanceState: Bundle?
-    ): View? {
+    override fun initViewBinding(
+        inflater: LayoutInflater,
+        container: ViewGroup?
+    ): FragmentGameBinding {
+        return FragmentGameBinding.inflate(inflater, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        setup()
+    }
+
+    private fun setup() {
         val categoryId = GameFragmentArgs.fromBundle(requireArguments()).categoryId
-        android.widget.Toast.makeText(
-            requireContext(),
-            "Category id: $categoryId",
-            android.widget.Toast.LENGTH_SHORT
-        ).show()
-
-        return inflater.inflate(R.layout.fragment_game, container, false)
+        // TODO: Make difficulty selectable
+        presenter.loadQuestions(12, categoryId, Difficulty.MEDIUM.value,)
     }
+
+    override fun showQuestions(questions: List<Question>) {
+        binding.questionText.text = questions[0].question
+    }
+
 
 }
